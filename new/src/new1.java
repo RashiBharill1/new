677fd3daf56ae2bab61243ccfd60ebbbe06a8bbd
Rashi <rashibharill6@gmail.com>
2020-03-08 00:38:05 -0800

    project

---
+++ new/src/new1.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class new1 {
+
+		public static void main(String[] args) {
+			ArrayList al = new ArrayList();
+			//Orange o=new Orange();
+
+			al.add("m1");
+			al.add("m2");
+			al.add("m3");
+			al.add("m4");
+			al.add("m5");
+			al.add("t1");
+			al.add("t2");
+			al.add("t3");
+			al.add("t4");
+			al.add("p1");
+			al.add("p2");
+		System.out.println(al);
+}
+}
